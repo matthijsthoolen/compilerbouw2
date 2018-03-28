8f71c9da06ecd2bbd4978719b451fc99bf857911
Matthijs Thoolen <matthijsthoolen@hotmail.com>
2018-03-28 14:22:23 +0200

    Remove cpp files

--- testAutomated/basic/check_error/global_statement.cvc.cpp
+++
@@ -1,9 +0,0 @@
-# 1 "basic/check_error/global_statement.cvc"
-# 1 "<built-in>"
-# 1 "<command-line>"
-# 31 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 32 "<command-line>" 2
-# 1 "basic/check_error/global_statement.cvc"
-int a;
-a = 1;
--- testAutomated/basic/check_success/comment_singleline.cvc.cpp
+++
@@ -1,9 +0,0 @@
-# 1 "basic/check_success/comment_singleline.cvc"
-# 1 "<built-in>"
-# 1 "<command-line>"
-# 31 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 32 "<command-line>" 2
-# 1 "basic/check_success/comment_singleline.cvc"
-
-int a;
